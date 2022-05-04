6353e88f03d463b6239dd4254f74391de0765832
Administrator <admin@example.com>
2022-05-04 17:19:23 +0800

    change 01-callback-func

--- 01-callback-function/main.go
+++ 01-callback-function/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 //callback function haha()
 func haha() {
+	defer wg.Done()
 	time.Sleep(time.Second * 5)
 	fmt.Println("Hello, my name is haha")
-	//wg.Done()
 }
 
 //callback function tony()
 func tony() {
+	defer wg.Done()
 	time.Sleep(time.Second * 5)
 	fmt.Println("Hello, my name is tony")
-	//wg.Done()
 }
 
 //args: callback function funcName(with or without arguments){}
@@ -26,20 +27,21 @@ func runner(funcName func()) {
 }
 
 //WaitGroup to make runner()  running background
-//var wg sync.WaitGroup
+var wg sync.WaitGroup
 
 func main() {
-	//fmt.Println("call to haha()")
-	//wg.Add(2)
-	//go runner(haha)
-	//fmt.Println("call to haha()")
-	//
-	//fmt.Println("call to tony()")
-	//go runner(tony)
-	//fmt.Println("call to tony()")
-	//wg.Wait()
-	runner(haha)
-	runner(tony)
+	fmt.Println("call to haha()")
+	wg.Add(1)
+	go runner(haha)
+	fmt.Println("call to haha()")
+
+	fmt.Println("call to tony()")
+	wg.Add(1)
+	go runner(tony)
+	fmt.Println("call to tony()")
+	wg.Wait()
+	//runner(haha)
+	//runner(tony)
 
 
 }
